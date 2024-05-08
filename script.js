0e7de510f48942af7cca74a1cbbd4b026bcfbf89
Eyüphan Kaygusuz <66624889+eyuphankygsz@users.noreply.github.com>
2024-05-08 11:38:13 +0300

    Update v0.9

--- script.js
+++ script.js
@@ -25,15 +25,7 @@ const pageContent = document.querySelector(PAGE_CONTENT_SELECTOR);
 const buttonHolder = document.querySelector(BUTTON_HOLDER_SELECTOR);
 const info = document.querySelector(INFO_SELECTOR);
 
-const arrangeButtons = () => {
-  let template = "";
-  for (let i = 0; i < buttonHolder.children.length; i++) {
-    if (i % 2 == 0) template += "auto ";
-    else template += "2px ";
-  }
-  buttonHolder.style.gridTemplateRows = template;
-};
-arrangeButtons();
+
 
 const pages = ["whoAmI", "projects", "skills", "business", "why"];
 
